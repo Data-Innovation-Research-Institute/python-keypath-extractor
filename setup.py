cd1c04532a14d17d888aab0c72658a62da93b44b
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-09-17 09:34:38 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="python-keypath-extractor",
+    version="0.0.1",
+    author="Jeffrey Morgan",
+    description="Extract Python dictionary values with keypaths.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DrJeffreyMorgan/python-keypath-extractor.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
